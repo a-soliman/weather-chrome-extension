11d5d33f70a59190ea9768ecfa049264d4d3ed7c
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-10-24 14:16:13 -0700

    added config file to hold the API key

---
+++ src/config/config_example.ts
@@ -0,0 +1,8 @@
+/**
+ * IMPORTANT: DUPLICATE THIS FILE AND SAVE THE DUPLICATED CONTENT AS 'config.ts', 
+ * then fille the <ADD YOU API KEY HERE> in the newly created file
+ */
+
+export const config = {
+  OPEN_WEATHER_API_KEY: '<ADD YOU API KEY HERE>',
+};
